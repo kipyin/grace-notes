a8314c024fd46cba6a0d61c3b2551707bb776738
Kip <28321392+kipyin@users.noreply.github.com>
2026-03-15 19:03:51 +0800

    Initial Commit

---
+++ FiveCubedMomentsTests/FiveCubedMomentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FiveCubedMomentsTests.swift
+//  FiveCubedMomentsTests
+//
+//  Created by Kip on 2026/3/15.
+//
+
+import Testing
+@testable import FiveCubedMoments
+
+struct FiveCubedMomentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
